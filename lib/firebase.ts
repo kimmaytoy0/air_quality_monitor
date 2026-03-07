84e008ff19c425d51ca08f90c11a4752bd95b629
Cha <kawlechaccyrz@gmail.com>
2026-03-07 19:26:09 +0800

    Firebase loc

---
+++ lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "../../firebase/app";
+import { getDatabase } from "../../firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDG1K7Cs1X2gU4FRAZFS7NfjGDxh7U1kxs",
+  authDomain: "airquality-36305.firebaseapp.com",
+  databaseURL: "https://airquality-36305-default-rtdb.asia-southeast1.firebasedatabase.app ",
+  projectId: "airquality-36305",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
